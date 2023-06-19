492f90daeeccf68a97f66097e729a7401b3fecf0
The Cheaterman <the.cheaterman@gmail.com>
2023-06-19 21:33:11 +0200

    Don't import Kivy before fixing the console thing

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 import os
 import sys
 
-from kivy.resources import resource_add_path
-
 if not sys.stderr:
     # https://github.com/pyinstaller/pyinstaller/issues/3503
     os.environ['KIVY_NO_CONSOLELOG'] = '1'
 
 if getattr(sys, 'frozen', False):
+    from kivy.resources import resource_add_path
     resource_add_path(sys._MEIPASS)
